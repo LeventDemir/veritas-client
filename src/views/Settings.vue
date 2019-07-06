1bb3c3bc9a675af8ee11f7dda3e4af8cd6843355
LeventDemir <leventdemir518@gmail.com>
2019-07-06 13:36:57 +0300

    settings

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -4,58 +4,102 @@
       <div class="col-xs-12 col-sm-10 col-md-12">
         <br />
         <br />
+        <br />
         <form @submit.prevent="save">
-          <input
-            type="email"
-            id="email"
-            v-model="settings.email"
-            required
-            placeholder="Email"
-            class="form-control"
-          />
-          <br />
+          <div class="form-group">
+            <div class="input-group">
+              <span class="input-group-addon">
+                <span class="glyphicon glyphicon-envelope"></span>
+              </span>
+              <input
+                type="email"
+                id="email"
+                v-model="settings.email"
+                required
+                placeholder="Email"
+                class="form-control noneradius"
+              />
+            </div>
+          </div>
 
-          <input
-            type="number"
-            id="phone"
-            v-model="settings.phone"
-            placeholder="Telefon"
-            required
-            class="form-control"
-          />
-          <br />
+          <div class="form-group">
+            <div class="input-group">
+              <span class="input-group-addon">
+                <span class="glyphicon glyphicon-phone"></span>
+              </span>
+              <input
+                type="number"
+                id="phone"
+                v-model="settings.phone"
+                placeholder="Telefon"
+                required
+                class="form-control noneradius"
+              />
+            </div>
+          </div>
 
-          <input
-            type="text"
-            id="instagram"
-            v-model="settings.instagram"
-            placeholder="Instagram"
-            class="form-control"
-          />
-          <br />
+          <div class="form-group">
+            <div class="input-group">
+              <span class="input-group-addon">
+                <span class="fab fa-instagram"></span>
+              </span>
+              <input
+                type="text"
+                id="instagram"
+                v-model="settings.instagram"
+                placeholder="Instagram"
+                class="form-control noneradius"
+              />
+            </div>
+          </div>
 
-          <input type="text" v-model="settings.twitter" placeholder="Twitter" class="form-control" />
-          <br />
+          <div class="form-group">
+            <div class="input-group">
+              <span class="input-group-addon">
+                <span class="fab fa-twitter"></span>
+              </span>
+              <input
+                type="text"
+                id="twitter"
+                v-model="settings.twitter"
+                placeholder="Twitter"
+                class="form-control noneradius"
+              />
+            </div>
+          </div>
 
-          <input
-            type="text"
-            id="facebook"
-            v-model="settings.facebook"
-            placeholder="Facebook"
-            class="form-control"
-          />
-          <br />
+          <div class="form-group">
+            <div class="input-group">
+              <span class="input-group-addon">
+                <span class="fab fa-facebook-f"></span>
+              </span>
+              <input
+                type="text"
+                id="facebook"
+                v-model="settings.facebook"
+                placeholder="Facebook"
+                class="form-control noneradius"
+              />
+            </div>
+          </div>
 
-          <textarea
-            id="address"
-            v-model="settings.address"
-            required
-            placeholder="Adres"
-            cols="30"
-            rows="5"
-            class="form-control"
-          />
-          <br />
+          <div class="form-group">
+            <div class="input-group">
+              <span class="input-group-addon">
+                <span class="fa fa-map-marker-alt"></span>
+              </span>
+              <textarea
+                type="text"
+                id="address"
+                v-model="settings.address"
+                required
+                placeholder="Adres"
+                cols="30"
+                rows="5"
+                class="form-control noneradius"
+              ></textarea>
+            </div>
+          </div>
 
           <button class="btn btn-danger" style="width:100%">Kaydet</button>
         </form>
