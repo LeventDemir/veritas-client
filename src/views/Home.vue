a14824665fd585c841baf9857ce3702b6476df21
LeventDemir <leventdemir518@gmail.com>
2019-07-09 13:41:45 +0300

    collapse

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -36,7 +36,7 @@
       </a>
     </div>
 
-    <!--Intro-Area-->
+    <!-- Categories -->
     <section class="relative">
       <div class="section-bg white"></div>
       <div class="container">
@@ -48,9 +48,8 @@
         </div>
       </div>
     </section>
-    <!--Intro-Area/-->
+    <!-- CategoriesI /-->
 
-    <!--Intro-Area-->
     <section class="relative">
       <div class="space-100 hidden-xs"></div>
       <div class="space-50"></div>
@@ -131,6 +130,281 @@
       <div class="space-100 hidden-xs"></div>
       <div class="space-50"></div>
     </section>
+
+    <div class="container">
+      <div class="space-100" />
+
+      <h1 class="text-center">Klima alırken nelere dikkate etmeliyim?</h1>
+      <div class="space-20" />
+
+      <hr />
+
+      <div class="accordion" id="accordionExample">
+        <div>
+          <button
+            id="headingTypes"
+            class="btn btn-link collapsed"
+            type="button"
+            data-toggle="collapse"
+            data-target="#types"
+            aria-expanded="false"
+            aria-controls="types"
+          >Klima Tipleri</button>
+
+          <div
+            id="types"
+            class="collapse"
+            aria-labelledby="headingTypes"
+            data-parent="#accordionExample"
+          >
+            <div class="container">
+              <div class="space-20"></div>
+
+              <h4>Hangi Klima Çeşitlerini Seçmelisiniz? Neden?</h4>
+              <p>Klima seçerken ilk adımda sizin için uygun olan klima tipine karar vermelisiniz. Klima tipleri, kullanacağınız ortama ve ihtiyaçlarınıza göre ayrılıyor. Klima çeşitleri arasından sağlıklı seçim yapmak için klimayı hangi amaçla ve nerede kullanacağınızı belirlemeniz şart.</p>
+              <hr />
+
+              <p>
+                <b>Duvar Tipi Klima</b>
+              </p>
+              <p>Duvar tipi klima, split klima ya da ev tipi klima olarak bilinen bu klimalar, iç ve dış ünite olmak üzere iki farklı parçadan oluşuyor. Bu klimaların çalışma prensipleri ise aslında oldukça basit. Soğutma ve ısıtma işlemi için kuruldukları mekândaki havayı çekiyorlar ve mevsime göre soğuk ya da sıcak hava üflüyorlar. Böylece içerideki hava daima serin kalmış oluyor. Duvara monte edildikleri için evde yer kaplamıyorlar ve hem soğutma hem de ısıtma işlemini rahatlıkla yapıyorlar.</p>
+
+              <div class="space-20"></div>
+
+              <p>
+                <b>Salon Tipi Klima</b>
+              </p>
+              <p>Salon tipi klimalar da iç ve dış ünite olmak üzere iki farklı bölümden oluşuyor ve klimanın iç ünitesi ayakta duruyor. Genellikle ofisler ya da bankalar gibi evlerden daha geniş alanlarda kullanılan salon tipi klimaların montajı için duvar ya da pencere gerekmediğinden bu tip klimalar, mekân içerisinde istenilen yere sabitlenebiliyor.</p>
+
+              <div class="space-20"></div>
+
+              <p>
+                <b>Mobil-Portatif Klima</b>
+              </p>
+              <p>Mobil klimalar daha düşük metrekareye sahip alanlarda kullanılabiliyor. Klimasını evin farklı alanlarında kullanmak isteyenler, kiracı olduğu için yaşadığı evde sabit klima istemeyenler ya da yaz aylarında yazlığa giden kullanıcılar için mobil-portatif klimalar oldukça uygun bir seçenek. Bebek ve çocuk odasının ekstra ısıtılmasına ihtiyaç duyulduğunda da kullanılabilen ve seyyar klima olarak da bilinen portatif klimalar, dış ünite montajının yapılamayacağı binalarda yaşayanlar için de oldukça kullanışlı.</p>
+            </div>
+          </div>
+        </div>
+
+        <hr />
+
+        <div>
+          <button
+            id="headingCapacity"
+            class="btn btn-link collapsed"
+            type="button"
+            data-toggle="collapse"
+            data-target="#capacity"
+            aria-expanded="false"
+            aria-controls="capacity"
+          >Kapasite</button>
+
+          <div
+            id="capacity"
+            class="collapse"
+            aria-labelledby="headingCapacity"
+            data-parent="#accordionExample"
+          >
+            <div class="container">
+              <div class="space-20"></div>
+              <h4>Klima İçin Çok Önemli İki Kavram: Kapasite ve BTU</h4>
+              <p>Hangi klima tipinin size uygun olduğuna karar verdiniz ama sadece bu adım doğru klimayı seçmeniz için yeterli değil. Sırada almak istediğiniz klima tipinin, klimayı konumlandırmak istediğiniz alan için yeterli olup olmadığını analiz etmek var. Bu aşamada, hesaplama yaparak ulaşacağınız değerler doğru klima seçimi için hayati bir önem taşıyor.</p>
+              <hr />
+
+              <p>
+                <b>Klima Kapasitesi Nedir?</b>
+              </p>
+              <p>
+                Klimalar farklı genişlikteki alanlar için çeşitli kapasitelerde üretiliyorlar ve bu nedenle klimanın kurulacağı alanı hesaplamak önemli.
+                Eğer klimanın kapasitesi, mekânın ihtiyacından küçükse verimli ısıtma/soğutma sağlanması mümkün olmuyor. Aynı şekilde klima, kurulacağı alan için fazla güçlüyse de istenen performansı elde edemeyebiliyor ya da yüksek faturalarla karşı karşıya kalabiliyorsunuz.
+                Ortamdaki kişi sayısı, mekânda kaç adet pencere olduğu, mekânın kuzey/güney cephelerinden hangisine baktığı, güneş ışınlarını alma derecesi, evin kaçıncı katta olduğu, hatta yaşanılan coğrafi bölge bile klima seçiminde önemli kriterler arasında yer alıyor. Örneğin; güney cepheye bakan, bol pencereli ve gün içinde doğrudan güneş ışınları alan bir odanın ısınması çok daha kolay olurken, kuzey cephelerdeki odalar kolaylıkla soğuyabiliyor.
+              </p>
+              <div class="space-20"></div>
+
+              <p>
+                <b>BTU Nedir? Klimada BTU Nasıl Hesaplanır?</b>
+              </p>
+              <p>Klimanın soğutma ve ısıtma gücünü belli eden bir değer olan BTU, alanın kapasitesi, ortamdaki kişi sayısı ve yaşadığınız coğrafi konum doğrultusunda sayısal bir sonuç sunuyor. Bu sonuç sayesinde, evinize uygun olan klimayı bulmanız mümkün hale geliyor. Peki, BTU nasıl hesaplanır?</p>
+              <p>Her coğrafi bölgenin özel bir BTU katsayısı bulunuyor. BTU hesaplaması yaparken yaşadığınız bölgenin katsayısı ile işlem yapmanız gerekiyor.</p>
+
+              <div class="space-10"></div>
+
+              <ul style="padding-left: 15px">
+                <li>Marmara Bölgesi: 385</li>
+                <li>İç Anadolu Bölgesi: 346</li>
+                <li>Ege Bölgesi: 423</li>
+                <li>Akdeniz Bölgesi: 445</li>
+                <li>Karadeniz Bölgesi: 385</li>
+                <li>Güneydoğu Anadolu Bölgesi: 462</li>
+                <li>Doğu Anadolu Bölgesi: 308</li>
+              </ul>
+
+              <div class="space-10"></div>
+
+              <p>BTU değerini bulmak için yaşadığınız coğrafi bölge katsayısı ile klimayı kullanmak istediğiniz alanın metrekaresini çarpmalısınız. Çıkan sonuca, evde yaşayan kişi sayısı kadar 600 eklemelisiniz. Bulduğunuz sonuç, klimayı kullanmak istediğiniz alanın BTU değerini gösteriyor. Eğer alanda 500 Watt üzeri aydınlatma kullanılıyorsa her Watt başına 3.412 eklemeyi unutmamalısınız ancak çoğu evde bu sınırın altında aydınlatma kullanılıyor.</p>
+
+              <div class="space-10"></div>
+
+              <p>
+                <b>Küçük bir örnekle BTU hesaplamak gerekirse:</b>
+              </p>
+              <p>Akdeniz Bölgesi, 4 kişilik bir aile, 20 metrekarelik alan.</p>
+              <p>445 (Akdeniz Bölgesi BTU katsayısı) x 20 (alanın metrekaresi) = 8900</p>
+              <p>600 (kişi başı eklenecek değer) x 4 (kişi sayısı) = 2400</p>
+              <p>8900 + 2400 = 11300</p>
+              <p>Bu sonuç doğrultusunda 12000 BTU değerine sahip klimalar tercih edilebilir.</p>
+            </div>
+          </div>
+        </div>
+
+        <hr />
+
+        <div>
+          <button
+            id="headingEnergy"
+            class="btn btn-link collapsed"
+            type="button"
+            data-toggle="collapse"
+            data-target="#energy"
+            aria-expanded="false"
+            aria-controls="energy"
+          >Enerji Tasarrufu</button>
+
+          <div
+            id="energy"
+            class="collapse"
+            aria-labelledby="headingEnergy"
+            data-parent="#accordionExample"
+          >
+            <div class="container">
+              <div class="space-20"></div>
+              <h4>Klimada Enerji Verimliliğini Artırmak İster Misiniz?</h4>
+              <p>Kapasite ve BTU değeri, alana uygun klima seçimini yapıp enerji verimliliğini sağlamak için oldukça etkili ama daha çok tasarruf etmenizi sağlayan başka özellikler de var. Bu teknolojiler sayesinde hem faturanız kabarmıyor hem de doğaya katkınız artıyor.</p>
+              <hr />
+
+              <p>
+                <b>Enerji Sınıfı: Artısı Var</b>
+              </p>
+              <p>Satın almadan önce dikkat edilmesi gereken noktalardan biri de klimanın enerji sınıfı. Bu cihazlar, enerji sınıflarına göre elektrik tükettikleri için doğru sınıfta cihaz alınmaması, faturanın kabarmasını beraberinde getiriyor.</p>
+              <p>Klimalarda A, A+, A++ ve A+++ olmak üzere dört enerji sınıfı bulunuyor. A+++’a yaklaştıkça verimlilik artıyor, enerji tüketimi azalıyor ve hem faturalarınız daha az geliyor hem de doğaya katkı sağlamış oluyorsunuz. Yani A sınıfı klimalarda ne kadar çok +, soğutmada o kadar çok verimlilik anlamına geliyor.</p>
+
+              <div class="space-20"></div>
+
+              <p>
+                <b>Inverter Teknolojisi: Enerji Tasarrufu Çalışmaktan Geçiyor</b>
+              </p>
+              <p>Inverter klimalar, on/off klimaların aksine, odanız istediğiniz sıcaklığa erişse bile durmuyor ve çok az enerji harcayarak çalışmaya devam ediyor. Bu teknoloji sayesinde siz ideal sıcaklığın keyfini sürerken klima bir durup bir çalışmadığı için enerji tasarrufu yapmak da mümkün hale geliyor.</p>
+              <p>On/off olarak adlandırılan klimalar ise alanda istediğiniz sıcaklığa ulaşmak için çalışıyor, bu sıcaklığa eriştiklerinde duruyor ve oda sıcaklığı değiştiğinde ise yeniden çalışmaya başlıyorlar. Odayı yeniden istenilen sıcaklığa getirebilmek için bir kez daha başlayan bu çalışma süreci ise enerji tüketiminin en yüksek olduğu aralık oluyor.</p>
+
+              <div class="space-20"></div>
+
+              <p>
+                <b>Aktif Enerji Kontrolü (Enerji Tasarruf Modu): Kontrol Sizde</b>
+              </p>
+              <p>Aktif enerji kontrolü size enerjiyi, odadaki kişi sayısı, sıcaklık koşulları gibi etkenlere göre kontrol edebilme imkânı sunuyor. Klima motorunun maksimum enerjiyle çalışmasının önüne geçen ve tasarruf yapmanızı sağlayan enerji tasarrufu modu sayesinde birinci kademede %20, ikinci kademede %40, üçüncü kademede ise %60 enerji tasarrufu sağlanabiliyor.</p>
+            </div>
+          </div>
+        </div>
+
+        <hr />
+
+        <div>
+          <button
+            id="headingHygiene-sound"
+            class="btn btn-link collapsed"
+            type="button"
+            data-toggle="collapse"
+            data-target="#hygiene-sound"
+            aria-expanded="false"
+            aria-controls="hygiene-sound"
+          >Hijyen ve Ses</button>
+
+          <div
+            id="hygiene-sound"
+            class="collapse"
+            aria-labelledby="headingHygiene-sound"
+            data-parent="#accordionExample"
+          >
+            <div class="container">
+              <div class="space-20"></div>
+              <h4>Daha Sessiz Bir Klima ve Daha Temiz Bir Hava: Ses Seviyesi ve Hijyen</h4>
+              <hr />
+
+              <p>
+                <b>İdeal Klima Ses Seviyesi Ne Olmalı?</b>
+              </p>
+              <p>Ofiste ya da odada saatlerce çalışacak bir klimanın sesinin fazla olmaması tercih edilir. Çünkü çok sesli çalışan bir klimanın vereceği rahatsızlık hissi, sunduğu tüm avantajları geride bırakabilir. Eğer siz de klimanızın daha da sessiz çalışmasını istiyorsanız, klima satın alırken dbA değerine dikkat edebilir ve sessiz mod özelliğine sahip modelleri tercih edebilirsiniz.</p>
+
+              <div class="space-20"></div>
+
+              <p>
+                <b>Ionizer+ Teknolojisi: Daha Kaliteli Bir Hava</b>
+              </p>
+              <p>Ionizer+ teknolojisi, hava kalitesini artıran bir teknoloji. Bu teknoloji sayesinde klimanız sadece size ideal sıcaklığı sunmakla kalmıyor aynı zamanda kaliteli bir hava da sunuyor. Ionizer+ teknolojisinin çalışma prensibi ise oldukça basit. Bu teknolojiye sahip klima, havada bulunan hidrojen ve oksijen sayesinde küçük iyon kümecikleri oluşturuyor. Oluşan bu pozitif parçacıklar odanızdaki negatif parçacıklarla karşılaşıyor, onların zararlı etkilerini yok ediyor ve bunu yaparken kesinlikle nemli bir hava oluşturmuyor.</p>
+
+              <div class="space-20"></div>
+
+              <p>
+                <b>Başka Hangi Hava Temizleme Teknolojileri Var?</b>
+              </p>
+              <p>
+                İdeal sıcaklığa ulaşmak ve enerji tasarrufu sağlayabilmek amacıyla klimanın bulunduğu odalar haliyle pek havalandırılmıyor. Peki, evinizin havasını çok daha kaliteli kılacak diğer özelliklere göz atmaya ne dersiniz?
+                Kolay Çıkarılabilir Toz Filtresi: İşinizi Kolaylaştırır
+                3 mikrometre (µm) üzerindeki tozları tutan ve ilk filtre olarak da bilinen toz filtreleri, ortama verilen havanın temiz olmasına ve alerjenlerden arınmasına katkı sağlıyor. Klima kapağının içinde değil, üzerinde bulunan bu toz filtresi adından da anlaşıldığı gibi kolaylıkla çıkıyor ve temizlenebiliyor. Bu nedenle klima alırken, bu tip filtreye sahip bir yeni nesil model seçmeniz oldukça önemli.
+                3M Filtre: Ondan Kaçmak Zor
+                İlk filtrenin yakalayamadığı, küçük boyuttaki partikülleri yakalayan bu özel filtre 3 mikrometre (µm)’den küçük toz parçacıklarını hapsederek temiz bir hava sunuyor.
+                Ionizer+ Otomotik Temizleme: Klimanız Kapalıyken de Çalışıyor
+                Odanızı soğuturken klimanın içerisinde oluşan nemi kurutan Ionizer+ otomatik temizleme özelliği, klima kapatıldıktan sonra devreye giriyor ve klimanın içerisinde oluşan nemi kurutuyor. Bu özellik sayesinde temiz havanın tadını daha uzun ömürlü bir klima ile daha uzun süre çıkarabiliyorsunuz.
+                Çift Yönlü Taze Hava Sistemi: Sadece Isıya Değil Daha Taze Havaya da Önem Verenlere Özel
+                Inverter klimalara takılabilen ve klimayla uyumlu olarak çalışan Çift Yönlü Taze Hava Sistemi, dışarıdan aldığı havayı temizliyor ve arındırıyor. Dakikada 500 litre havayı temizleyerek odanın havalandırılmasına olan ihtiyacı da ortadan kaldırıyor. Üstelik duşlama tekniği adı verilen özel çalışma prensibi sayesinde taze havayı odaya eşit bir şekilde dağıtmayı da başarıyor. Bu da mekânın havası temizlenirken ısısının değişmemesini sağlıyor. Ayrıca Duo Fresh Air’in egzoz modu çalıştırıldığında da ortamdaki duman ve koku da dışarı atılıyor.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <hr />
+
+        <div>
+          <button
+            id="headingComfort"
+            class="btn btn-link collapsed"
+            type="button"
+            data-toggle="collapse"
+            data-target="#comfort"
+            aria-expanded="false"
+            aria-controls="comfort"
+          >Konfor</button>
+
+          <div
+            id="comfort"
+            class="collapse"
+            aria-labelledby="headingComfort"
+            data-parent="#accordionExample"
+          >
+            <div class="container">
+              <div class="space-20"></div>
+              <h4>Yaşam Kalitenizi Artıracak Teknolojiler</h4>
+              <p>Klimaların sahip olduğu yararlı özelliklerle hem tasarruf edebilir hem de klimanızı konforlu bir şekilde kullanabilirsiniz. Şimdi o özellikleri tanımaya başlayalım.</p>
+              <hr />
+
+              <p>
+                <b>Uzaklığı Engel Olmaktan Çıkarın: Uzaktan Kontrol Wi-Fi (Connect)</b>
+              </p>
+              <p>İnternet bağlantısı olan telefon, tablet gibi cihazlarınız sayesinde kullanabileceğiniz uzaktan kontrol Wi-Fi teknolojisi sayesinde, klimanızın olduğu alanda olmasanız bile onu çalıştırmanız mümkün hale geliyor. Bu fonksiyonla evinize ya da iş yerinize gitmeden alanı soğutabiliyor ya da ısıtabiliyorsunuz. Normal şartlar altında odaya geldiğinizde açtığınız klima, ideal sıcaklığı yakalayabilmek için yüksek enerji harcarken uzaktan kontrol Wi-Fi (Connect) özelliği sayesinde düşük seviyede çalışmaya başlayarak hem konforunuzu sağlıyor hem de enerji tasarrufu yapıyor.</p>
+
+              <div class="space-20"></div>
+
+              <p>
+                <b>Kaliteli Uykunun ve Enerji Tasarrufunun Sırrı: Uyku Modu ve Otomatik Açma/Kapama</b>
+              </p>
+              <p>Uyku modu, siz uyurken odanızın çok ısınmasını ve soğumasını engelliyor ve ideal sıcaklığın korunmasına yardımcı oluyor. Otomatik açma/kapama özelliği sayesinde ise klimanızın açılma ve kapanma saatlerini belirleyebilirsiniz.</p>
+            </div>
+          </div>
+        </div>
+
+        <hr />
+      </div>
+    </div>
   </div>
 </template>
 
