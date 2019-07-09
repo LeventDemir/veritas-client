4e7088f51b39c42530c4f6b22da3e6be657d7822
LeventDemir <leventdemir518@gmail.com>
2019-07-09 11:42:30 +0300

    images

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,7 @@
       <div class="row">
         <div class="col-xs-12 col-sm-4" style="color: #FFFFFF;">
           <img
-            src="/src/assets/img/diamondelectric_white.png"
+            src="/src/assets/img/logo/diamondelectric_white.png"
             alt
             style="filter: brightness(0) invert(1);"
           />
@@ -44,8 +44,10 @@
               </a>
             </li>
           </ul>
+
           <div class="space-30"></div>
         </div>
+
         <div class="col-md-offset-1 hidden-xs hidden-sm col-md-2">
           <div class="single-footer">
             <div class="space-10"></div>
@@ -66,6 +68,7 @@
           </div>
           <div class="space-30"></div>
         </div>
+
         <div class="hidden-xs hidden-sm col-sm-5">
           <div class="single-footer">
             <div class="space-10"></div>
@@ -93,13 +96,13 @@
           <div class="single-footer">
             <div class="space-10"></div>
             <div class="col-md-offset-1 col-xs-12 col-sm-12 col-md-4 col-lg-4">
-              <img src="/src/assets/img/delpha.png" />
+              <!-- <img src="/src/assets/img/logo/delpha.png" /> -->
             </div>
             <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-              <img src="/src/assets/img/visa-master-logolari.png" />
+              <img src="/src/assets/img/logo/visa-master-logolari.png" />
             </div>
             <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
-              <img src="/src/assets/img/comodo-secure-logo-new.png" />
+              <img src="/src/assets/img/logo/comodo-secure-logo-new.png" />
             </div>
           </div>
           <div class="space-100"></div>
