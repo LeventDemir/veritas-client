53d91f5acaec27e421f5d9d7771688459b3daf9a
LeventDemir <leventdemir518@gmail.com>
2019-07-09 08:48:28 +0300

    abour

--- src/views/About.vue
+++ src/views/About.vue
@@ -14,10 +14,18 @@
           </div>
 
           <p>
-            Kaliteli ürünleri ve konusundaki uzmanlığı ile öne çıkan Diamond Electric sizlere konforlu ve
-            sağlıklı bir iklimlendirme sağlıyor.
+            1965 senesinde başlayan mühendislik tecrübemiz ile önde gelen mimarlara, gayrimenkul geliştiricilerine, üretim tesislerine ve kamu kuruluşlarına kapsamlı yapısal inşaat mühendisliği hizmetleri sağlamaktayız.
+            Veritas, Latince'de "hakikat, doğruluk" anlamına gelir. Antik Roma'da doğruluk erdemine de verilen isim olan Veritas, aynı zamanda mitoloji de hakikatin vücut bulmuş halidir.
+            Gerçek bilgi ve doğruluğu takip etme inancımız bize bilim ve toplum arasında köprüler kurmamızı sağlamaktadır.
+            Mühendislik tasarım ekibimiz, zarif detaylar tasarlayabildiği gibi pragmatik kararlar veren hem analitik ve hem de yaratıcı mühendislik birikiminin harmanıdır.
+            Yapısal,deprem ve inşaat mühendisliği alanlarındaki uzmanlığımızla yapısal analiz, geoteknik , yüksek bina tasarımı , sismik tehlike vb. tüm yapı tasarım konuları çalışma kapsamımızdadır.
+            Mühendislik Yaklaşımımız
+            Mühendislik yaklaşımımız ; Fonksiyonellik, Sürdürülebilirlik, İnşa edilebilirlik, Yapı Güvenliği kriterlerinin hedeflenen bütçe ve iş programına uygun olarak büyük ölçekli projelerden, en ufak bir detay tasarımına kadar aynı özenle gerçekleştirilmesine dayanmaktadır.
+            Otomotiv, gıda, gemi üretim sanayisi, gayrimenkul ve kamu sektörlerindeki yıllara dayalı başarılı çalışmalarımız her yeni tasarladığımız yapıya tecrübemizi aktarmamıza neden olmaktadır.
+            Çalıştığımız her projede öncelikle arkasındaki ihtiyaçları anlamak isteriz. Bu bazen işveren’in ihtiyaçları , mimarın zihinsel ve mekânsal kavramı , çevresel etkiler ve bazen de müteahhit firmanın inşa yöntemleri olabilir. Böylece yapısal tasarımlarımız ve çözümlemelerimizle paydaşlar arası bir başarı ortaya çıkar.
+            Ekibimiz bu işbirliğine dayalı çalışma yaklaşımımızdan keyif alarak, tasarım ekibi olmanın ötesinde işveren , mimarlar ve yükleniciler ile bütünlük içerisinde hareket etmektedir.
+            Tanınmış gayrimenkul firmalarının binlerce ünitelik toplu konut projelerinden özgün bir adetlik şehir anıtına , dünya devi markaların büyük alanlı üretim tesislerinden özel bir müşterinin bir adetlik müstakil konutu ölçek aralığında sayısız projeler gerçekleştirdik. Her türde yapısal malzeme kullanılarak , geleneksel ve geleneksel olmayan tüm yapı tipleri mühendislik tasarımı çalışma kapsamımızdadır.
           </p>
-          <p>Kaliteli ve ekonomik çözümlerimizde kurumsal ve bireysel ihtiyaçlarınızın çözümü burada...</p>
         </div>
       </div>
     </div>
