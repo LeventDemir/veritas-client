cdb8e8c4058461cbb867fdbca608049c5c874cbb
LeventDemir <leventdemir518@gmail.com>
2019-07-22 14:21:39 +0300

    :)

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,7 +8,8 @@
           <div class="space-10"></div>Konacık Mh. Atatürk Blv. No: 200 Bodrum - Muğla / TURKEY
           <div class="clearfix"></div>
           <hr />
-          <a style="color: #FFFFFF;">ozden@veritasmuhendislik.com</a>
+          <a style="color: #FFFFFF;">özge@veritasmuhendislik.com</a>
+          <a style="color: #FFFFFF;">info@veritasmuhendislik.com</a>
           <br />+90 252 363 73 76
           <div class="space-10"></div>
           <ul class="list-inline list-unstyled social-list">
@@ -84,6 +85,10 @@
             </div>
             <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
               <img src="/src/assets/img/logo/mitsubishi-klima-logo.jpg" />
+            </div>
+            <div class="space-20"></div>
+            <div class="col-md-offset-1 col-xs-12 col-sm-12 col-md-4 col-lg-4">
+              <img src="/src/assets/img/logo/panasonic.png" />
             </div>
           </div>
           <div class="space-100"></div>
